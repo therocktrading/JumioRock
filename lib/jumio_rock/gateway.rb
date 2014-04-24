3f2fcd29bb0a0b93cec73e7e2413bd0afd0b0974
Michele Minazzato <michelemina@gmail.com>
2014-04-24 11:30:26 +0200

    basic authentication in url

--- lib/jumio_rock/gateway.rb
+++ lib/jumio_rock/gateway.rb
@@ -7,7 +7,8 @@ module JumioRock
     end
 
     def call(body)
-      connection = Excon.new(endpoint_url, :user => conf.api_token, :password => conf.api_secret)
+      connection = Excon.new("#{conf.api_token}:#{conf.api_secret}@#{endpoint_url}")
+      #connection = Excon.new(endpoint_url, :user => conf.api_token, :password => conf.api_secret)
       response = connection.request(
         method: 'post',
         headers: headers,
