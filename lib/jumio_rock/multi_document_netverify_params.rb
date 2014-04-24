72c50662e827db578b5439f31032ccacb70e80da
Michele Minazzato <michelemina@gmail.com>
2014-04-24 16:31:13 +0200

    fix initialize

--- lib/jumio_rock/multi_document_netverify_params.rb
+++ lib/jumio_rock/multi_document_netverify_params.rb
@@ -6,11 +6,11 @@ module JumioRock
     attr_accessor :authorizationTokenLifetime, :merchantReportingCriteria, :callbackUrl, :clientIp
 
     def initialize(document_type, merchant_scan_reference, customer_id, success_url, error_url)
-      documentType = document_type
-      merchantScanReference = merchant_scan_reference
-      customerID = customer_id
-      successUrl = success_url
-      errorUrl = error_url
+      @documentType = document_type
+      @merchantScanReference = merchant_scan_reference
+      @customerID = customer_id
+      @successUrl = success_url
+      @errorUrl = error_url
     end
 
   end
