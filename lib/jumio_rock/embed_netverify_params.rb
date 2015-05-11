e9405b8b3d79b4b452b1b69f642432a727282d49
Davide Barbieri <paci@therocktrading.com>
2015-05-11 11:26:26 +0200

    add new captureMethod field

--- lib/jumio_rock/embed_netverify_params.rb
+++ lib/jumio_rock/embed_netverify_params.rb
@@ -5,7 +5,7 @@ module JumioRock
     attr_reader :merchantIdScanReference, :successUrl, :errorUrl
     attr_accessor :enabledFields, :authorizationTokenLifetime, :merchantReportingCriteria, 
       :callbackUrl, :locale, :clientIp, :customerId, :firstName, :lastName, :country, 
-      :usState, :expiry, :number, :dob, :idType, :personalNumber, :mrzCheck, :additionalInformation
+      :usState, :expiry, :number, :dob, :idType, :personalNumber, :mrzCheck, :additionalInformation, :captureMethod
 
     def initialize(scan_reference, success_url, error_url)
       @merchantIdScanReference = scan_reference
