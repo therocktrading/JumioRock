6a483eb93a60d4a23d16ab4cff11eeb3adb246a2
Backnol Yogendran <backnol@gmail.com>
2016-05-03 17:05:27 -0400

    Add authorizationTokenLifetime for RedirectNetverifyParams.

--- lib/jumio_rock/redirect_netverify_params.rb
+++ lib/jumio_rock/redirect_netverify_params.rb
@@ -1,14 +1,11 @@
 module JumioRock
-  
-  class RedirectNetverifyParams < NetverifyParams 
-    
+  class RedirectNetverifyParams < NetverifyParams
     attr_reader :merchantIdScanReference, :customerId
+    attr_accessor :authorizationTokenLifetime
 
     def initialize(scan_reference, customer_id)
       @merchantIdScanReference = scan_reference
       @customerId = customer_id
     end
-
   end
-
 end
