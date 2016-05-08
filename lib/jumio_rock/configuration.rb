17afc5523ac6a2857425a5f2db1b1340ef8e9c8e
Backnol Yogendran <backnol@gmail.com>
2016-05-07 23:57:09 -0400

    Retrieval API integration.

--- lib/jumio_rock/configuration.rb
+++ lib/jumio_rock/configuration.rb
@@ -1,6 +1,7 @@
 module JumioRock
   class Configuration
-    attr_accessor :app_name, :company_name, :version, :api_url, :api_token, :api_secret, :init_redirect_url, :init_embed_url, :multi_document_url
+    attr_accessor :app_name, :company_name, :version, :api_url, :api_token, :api_secret, :init_redirect_url, :init_embed_url, :multi_document_url,
+                  :retrieval_url
 
     def initialize
       self.company_name = 'YOURCOMPANYNAME'
@@ -10,6 +11,7 @@ module JumioRock
       self.init_redirect_url = "https://netverify.com/api/netverify/v2/initiateNetverifyRedirect"
       self.init_embed_url = "https://netverify.com/api/netverify/v2/initiateNetverify"
       self.multi_document_url = "https://netverify.com/api/netverify/v2/createDocumentAcquisition"
+      self.retrieval_url = "https://netverify.com/api/netverify/v2/scans"
     end
 
     def self.configuration
